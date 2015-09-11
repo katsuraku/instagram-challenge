8b444b9096df7d3c865aa18776be6f76c49d981e
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-09-11 11:52:18 +0100

    Adds spec file for comment creator service

---
+++ spec/services/comment_creator_spec.rb
@@ -0,0 +1,17 @@
+describe CommentCreator do
+  # 
+  # let(:comment_params) { double { my_comment: 'whatever', current_user: 'something', picture_id: 1 }}
+  #
+  # comment_params = params.require(:comment).permit(:my_comment)
+  # comment_params[:current_user] = current_user
+  # comment_params[:picture_id] = params[:picture_id]
+  #
+  #
+  # subject(:comment_creator) do
+  #   CommentCreator.call(comment_params)
+  # end
+  #
+  # it 'creates a comment' do
+  #   expect(CommentCreator.call(comment_params)).to eq()
+  # end
+end
